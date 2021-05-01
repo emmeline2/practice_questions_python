a0f654a6bfd059f38e6cbfd2c67f33bf931ddc77
emmeline2 <elp65@case.edu>
2021-05-01 10:21:57 -0400

    Update MaxBuySell.py
    one loop solution

--- MaxBuySell.py
+++ MaxBuySell.py
@@ -8,15 +8,14 @@ class Solution(object):
             return 0
         
         max_profit = 0
-        index = 1;
+        # max int value
+        minprice = sys.maxint
         
-        # Brute force approach, iterate through every combination and 
-        # check if price is greater than the max profit, only check
-        # sell conditions that happen after the buy
-        for buy_price in prices:
-            for sell_index in range(index, len(prices)):
-                if prices[sell_index] - buy_price >  max_profit:
-                    max_profit = prices[sell_index] - buy_price
-            index = index + 1
+        # one pass with checking min against max values
+        for item in prices:
+            if item < minprice:
+                minprice = item
+            elif item - minprice > max_profit:
+                max_profit = item - minprice
         
         return max_profit
