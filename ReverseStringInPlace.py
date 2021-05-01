98131fab4b65cc5342fb1fba2eca7bd20eb72b77
emmeline2 <elp65@case.edu>
2021-05-01 10:46:13 -0400

    Update ReverseStringInPlace.py
    More complete solution

--- ReverseStringInPlace.py
+++ ReverseStringInPlace.py
@@ -4,5 +4,16 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        # reverses the string
-        s.reverse()
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            # swap left and right
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            # increment left
+            left += 1
+            # decrement right
+            right -= 1
+    
