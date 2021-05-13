81006edeb6299ea3d4facf83eb8b550a870ec3c7
emmeline2 <elp65@case.edu>
2021-05-13 19:11:29 -0400

    Create romanToInt.py
    Initial convert roman number to integer

---
+++ romanToInt.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        if s is None:
+            return 0
+        
+        value = 0
+        sum = 0
+        prev = 20000 # greater than max val
+ 
+        for char in s:
+            if char == "I":
+                value = 1
+            elif char == "V":
+                value = 5
+            elif char == "X":
+                value = 10
+            elif char == "L":
+                value = 50
+            elif char == "C":
+                value = 100
+            elif char == "D":
+                value = 500
+            elif char == "M":
+                value = 1000
+            else:
+                print "Invalid value"
+            
+            if value > prev:
+                # remove prev written value and next
+                sum = sum - (prev*2)
+                sum = sum + value
+            else:
+                sum = sum + value
+            print sum
+            
+            prev = value
+        
+        
+        return sum
