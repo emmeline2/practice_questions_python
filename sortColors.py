4cd949cacf639ee04f42343d195a6ca95e1b33cc
emmeline2 <elp65@case.edu>
2021-05-31 21:02:57 -0400

    Create sortColors.py

---
+++ sortColors.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        k = len(nums)-1
+        j = 0
+        while j <= k:
+            if i < j and nums[j] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            elif nums[j] == 2:
+                nums[j], nums[k] = nums[k], nums[j]
+                k -= 1
+            else:
+                j += 1
