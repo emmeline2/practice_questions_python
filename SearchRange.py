b39ae94da9f48990c26d6afcac48ebd01d53256b
emmeline2 <elp65@case.edu>
2021-06-01 20:42:37 -0400

    Create SearchRange.py
    initial

---
+++ SearchRange.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if target is None or nums is None:
+            return [-1, -1]
+        
+        retList = [-1, -1]
+    
+        for index in range(len(nums)):
+            if nums[index] == target and retList[0] == -1:
+                retList[0] = index
+                retList[1] = index
+            elif nums[index] == target: 
+                retList[1] = index
+        
+        
+        return retList
