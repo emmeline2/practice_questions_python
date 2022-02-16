a458a445966c1eebd583b27b0ddbaabba8284c59
Emmeline <elp65@case.edu>
2022-02-16 08:23:55 -0500

    Update FirstBadVersion.py
    Using binary search

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -14,9 +14,17 @@ class Solution(object):
         if n == None: 
             return None
         
-        for item in range(1, n): 
-            if isBadVersion(item): 
-                return item
+        left = 1
+        right = n
         
-        # catch the case of last item is bad
-        return n
+        while left <= right:
+            # get pivot value
+            pivot = left + (right - left) // 2
+            if isBadVersion(pivot): 
+                right = pivot -1 
+            else:
+                left = pivot + 1
+        
+        return left
+            
+            
