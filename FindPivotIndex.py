0d1cde90ebcaa7fd841549930cb55dab487e8554
Emmeline <elp65@case.edu>
2022-08-13 12:24:57 -0400

    Update FindPivotIndex.py
    O(N) time complexity using total sum of numbers

--- FindPivotIndex.py
+++ FindPivotIndex.py
@@ -13,6 +13,12 @@ class Solution(object):
         running sum from right
         [28,27,20,17,11,6]
         Pivot = index 3 (17)
+        
+        Given:
+        S is total sum of numbers
+        - At index i, the sum of all left numbers + number at i + right sum = total sum 
+        - So, total sum - number at i - sum of left numbers = right sum 
+        - So, only need total sum and current running left sum to check if it is the pivot
         """
         
         # input checks ---------------------------
@@ -21,31 +27,16 @@ class Solution(object):
         if len(nums) is 1:
             return 0
 
-            
-        # calculate right running sum ---------------------
-        # create a copy of nums
-        right_sum = nums[:]
+        # calculate pivot -------------------------
+        total_sum = sum(nums)
+        left_sum = 0
         
-        # range from end(excluding last) -> 0 (range is non inclusive), decrementing
-        for i in range(len(nums)-2, 0-1, -1): 
-            temp_sum = right_sum[i+1] + nums[i]
-            right_sum[i] = temp_sum
-        
-
-        # calculte left running sum  ------------------------
-        left_sum = []
-        left_sum.append(nums[0])
-        # check if zero index is the pivot
-        if left_sum[0] == right_sum[0]:
-                return 0
-        for i in range(1, len(nums)): 
-            temp_sum = left_sum[i-1] + nums[i]
-            left_sum.append(temp_sum)
-    
-            # check if pivot value by comparing against right
-            if left_sum[i] == right_sum[i]:
+        for i in range(0, len(nums)):
+            if left_sum == (total_sum - left_sum - nums[i]):
                 return i
+            left_sum = left_sum + nums[i]
+
         
-        # no index is found
+        # no index is found ------------------------
         return -1
             
